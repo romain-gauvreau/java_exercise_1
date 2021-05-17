fecaea744c79a962e9416ff14e6723635fa1fea1
Romain GAUVREAU <romain.gauvreau@epita.fr>
2021-05-17 16:52:05 +0200

    update loop

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,34 +1,30 @@
 import java.util.Scanner;
 
 public class Launcher {
-    public  static int fibo(int n)
-    {
+    public static int fibo(int n) {
         if (n <= 1)
             return n;
         return fibo(n - 1) + fibo(n - 2);
     }
 
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter command:");
-        String inputCommand = myObj.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        boolean shouldQuit = false;
+        do {
+            System.out.println("Please, input a command:");
+            String command = scanner.nextLine();
+            if ("quit".equals(command)) {
+                shouldQuit = true;
+                break;
+            } else if ("fibo".equals(command)) {
+                System.out.println("Input a number:");
+                int n = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println(fibo(n));
+            } else {
+                System.out.println("Unknown command");
+            }
+        } while (!shouldQuit);
 
-        String quitCommand = "quit";
-
-        while(!inputCommand.equals(quitCommand))
-        {
-           if(inputCommand.equals("fibo"))
-           {
-               System.out.println("Enter number:");
-               int number = myObj.nextInt();
-               myObj.nextLine();
-               int result = fibo(number);
-               System.out.println(result);
-           }
-           else {
-               System.out.println("Unknown command");
-           }
-            inputCommand = myObj.nextLine();
-        }
     }
 }
