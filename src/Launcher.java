70163bbcf0bd2347918c3c05d2796a2b47e1553b
Romain GAUVREAU <romain.gauvreau@epita.fr>
2021-05-17 15:15:07 +0200

    add part4: fibo command

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
 
 public class Launcher {
+    public  static int fibo(int n)
+    {
+        if (n <= 1)
+            return n;
+        return fibo(n - 1) + fibo(n - 2);
+    }
+
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         System.out.println("Enter command:");
@@ -10,7 +17,17 @@ public class Launcher {
 
         while(!inputCommand.equals(quitCommand))
         {
-            System.out.println("Unknown command");
+           if(inputCommand.equals("fibo"))
+           {
+               System.out.println("Enter number:");
+               int number = myObj.nextInt();
+               myObj.nextLine();
+               int result = fibo(number);
+               System.out.println(result);
+           }
+           else {
+               System.out.println("Unknown command");
+           }
             inputCommand = myObj.nextLine();
         }
     }
