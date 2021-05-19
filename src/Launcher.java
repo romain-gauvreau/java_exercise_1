5e30815fd627473b53296edaf2e3360d0ce10bf6
Romain GAUVREAU <romain.gauvreau@epita.fr>
2021-05-19 15:29:17 +0200

    add part6: refactoring

--- src/Launcher.java
+++ src/Launcher.java
@@ -7,43 +7,49 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Launcher {
-    public static int fibo(int n) {
+interface Command {
+    String name();
+    Boolean run(Scanner scanner);
+}
+
+class Quit implements Command {
+    @Override
+    public String name() {
+        return "quit";
+    }
+
+    @Override
+    public Boolean run(Scanner scanner) {
+        return true;
+    }
+}
+
+class Fibo implements Command {
+    @Override
+    public String name() {
+        return "fibo";
+    }
+
+    private static int fibo(int n) {
         if (n <= 1)
             return n;
         return fibo(n - 1) + fibo(n - 2);
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        boolean shouldQuit = false;
-        do {
-            System.out.println("Please, input a command:");
-            String command = scanner.nextLine();
-            if ("quit".equals(command)) {
-                shouldQuit = true;
-                break;
-            } else if ("fibo".equals(command)) {
-                System.out.println("Input a number:");
-                int n = scanner.nextInt();
-                scanner.nextLine();
-                System.out.println(fibo(n));
-            } else if ("freq".equals(command)) {
-                try {
-                    System.out.println("Input a file path:");
-                    String path = scanner.nextLine();
-                    Path filePath = Paths.get(path);
-                    String content = Files.readString(filePath);
-                    String[] words = content.split(" ");
-                    displayThreeMostOccurringWords(words);
-                } catch (IOException e) {
-                    System.out.println("Unreadable file: " + e.getClass() + e.getMessage());
-                }
-            } else {
-                System.out.println("Unknown command");
-            }
-        } while (!shouldQuit);
+    @Override
+    public Boolean run(Scanner scanner) {
+        System.out.println("Input a number:");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println(fibo(n));
+        return false;
+    }
+}
 
+class Freq implements Command {
+    @Override
+    public String name() {
+        return "freq";
     }
 
     private static void displayThreeMostOccurringWords(String[] words) {
@@ -65,5 +71,50 @@ public class Launcher {
                 .collect(Collectors.joining(" "));
 
         System.out.println(threeMostOccuringWords);
+    }
+
+    @Override
+    public Boolean run(Scanner scanner) {
+        try {
+            System.out.println("Input a file path:");
+            String path = scanner.nextLine();
+            Path filePath = Paths.get(path);
+            String content = Files.readString(filePath);
+            String[] words = content.split(" ");
+            displayThreeMostOccurringWords(words);
+        } catch (IOException e) {
+            System.out.println("Unreadable file: " + e.getClass() + e.getMessage());
+        }
+        return false;
+    }
+}
+
+public class Launcher {
+    public static void main(String[] args) {
+        List<Command> commandList = new ArrayList<Command>();
+        commandList.add(new Freq());
+        commandList.add(new Fibo());
+        commandList.add(new Quit());
+
+        Scanner scanner = new Scanner(System.in);
+        boolean shouldQuit = false;
+        boolean commandFound = false;
+
+        do {
+            System.out.println("Please, input a command:");
+            String command = scanner.nextLine();
+            for (Command current : commandList) {
+                if (current.name().equals(command)) {
+                    commandFound = true;
+                    shouldQuit = current.run(scanner);
+                    break;
+                }
+            }
+            if (commandFound) {
+                commandFound = false;
+            } else {
+                System.out.println("Command unknown");
+            }
+        } while (!shouldQuit);
     }
 }
